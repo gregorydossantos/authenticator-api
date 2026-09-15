3b4e7d8cda94e7cd1c863479d7a472569e1502cb
Gregory <gregescribano@gmail.com>
2026-09-15 16:01:34 -0300

    Initial commit

---
+++ src/main/java/com/gregory/authenticatorapi/AuthenticatorapiApplication.java
@@ -0,0 +1,13 @@
+package com.gregory.authenticatorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticatorapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticatorapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gregory/authenticatorapi/AuthenticatorapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gregory.authenticatorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatorapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
